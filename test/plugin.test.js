29786cf12cdacb1f8441c13af26a329d51d7da16
Almir Filho <almirflorenciofilho@gmail.com>
2016-05-03 18:24:02 -0300

    Renames several things in tests

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const metalsmith = require('metalsmith');
 const collections = require('metalsmith-collections');
-const feed = require('../src/plugin');
+const author = require('../src/plugin');
 
 
 describe('Initialization', () => {
@@ -10,54 +10,54 @@ describe('Initialization', () => {
   before(() => metal = metalsmith('test/fixtures'));
 
   it('should be a function', () => {
-    expect(feed).to.be.a.function;
+    expect(author).to.be.a.function;
   });
 
   it('should return a function', () => {
-    expect(feed({ collection: 'something' })).to.be.a.function;
+    expect(author({ collection: 'something' })).to.be.a.function;
   });
 
   it('should require collection name', () => {
-    expect(feed).to.throw(Error);
+    expect(author).to.throw(Error);
   });
 
   it('should require collections to be configured', () => {
     metal._metadata = {};
-    const call = feed({ collection: 'name' }).bind(null, [], metal, () => {});
+    const call = author({ collection: 'name' }).bind(null, [], metal, () => {});
     expect(call).to.throw(Error);
   });
 
   it('should throw error if collection doesnt exist', () => {
     metal._metadata.collections = {};
-    const call = feed({ collection: 'posts' }).bind(null, [], metal, () => {});
+    const call = author({ collection: 'posts' }).bind(null, [], metal, () => {});
     expect(call).to.throw(Error);
   });
 
   it('should not throw error if collection exist', () => {
     metal._metadata.collections = { articles: [] };
-    const call = feed({ collection: 'articles' }).bind(null, [], metal, () => {});
+    const call = author({ collection: 'articles' }).bind(null, [], metal, () => {});
     expect(call).not.to.throw(Error);
   });
 
   it('should throw error if authors option is not an object', () => {
     metal._metadata.collections = { posts: [] };
     const options = { collection: 'posts', authors: 'not an object' };
-    const call = feed(options).bind(null, [], metal, () => {});
+    const call = author(options).bind(null, [], metal, () => {});
     expect(call).to.throw(Error);
   });
 });
 
 
 describe('Generation', () => {
-  var metalfeed = feedoptions => (
+  var metalpipeline = authoroptions => (
     metalsmith('test')
       .source('fixtures')
       .use(collections({ posts: '*.html' }))
-      .use(feed(feedoptions))
+      .use(author(authoroptions))
   );
 
   it('should do nothing with files', done => {
-    metalfeed({ collection: 'posts' }).build((err, files) => {
+    metalpipeline({ collection: 'posts' }).build((err, files) => {
       for(var file in files){
         expect(files[file]).to.not.have.deep.property('author.name');
       }
@@ -74,7 +74,7 @@ describe('Generation', () => {
       }
     };
 
-    metalfeed(options).build((err, files) => {
+    metalpipeline(options).build((err, files) => {
       expect(files['post1.html']).to.have.property('author').to.deep.equal({ name: 'John Lennon' });
       expect(files['post2.html']).to.have.property('author').to.deep.equal({ name: 'Paul McCartney' });
       done();
@@ -89,7 +89,7 @@ describe('Generation', () => {
       }
     };
 
-    metalfeed(options).build((err, files) => {
+    metalpipeline(options).build((err, files) => {
       expect(files['post1.html']).to.have.property('author').to.deep.equal({ name: 'John Lennon' });
       expect(files['post2.html']).to.have.property('author').to.equal('paul');
       done();
@@ -103,7 +103,7 @@ describe('Generation', () => {
       authors: { john: 'John Lennon' }
     };
 
-    metalfeed(options).use((files, metal, metaldone) => {
+    metalpipeline(options).use((files, metal, metaldone) => {
       metadata = metal.metadata();
       metaldone();
     }).build((err, files) => {
